43cfdaf9a0b1743173adc1da3be18da0b2bbcfb7
Miguel Herranz <miguel@midokura.com>
2018-08-31 15:38:33 +0200

    Run unit tests when building docker images
    This will run the unit tests at build time. To easier integration
    with CI tools, a JUnit file is generated using go2xunit and copied
    from the intermediate `builder` container. Since it is needed a
    successful build to be able to recover the JUnit report, failing
    tests are ignored in `builder` container build. The error is raised
    in a subsequent `test_fail_checker` auxiliar container that repeats
    the go2xunit conversion in 'error on fail mode'. In case the build
    is a cross compilation for another arch, the tests will be not
    executed, but the build process will succeed as there will be no
    tests results (neither pass not fail).
    
    Change-Id: I9f4a2a0ad83a7c42ca3e9be94fa61d072d4959c1
    Signed-off-by: Miguel Herranz <miguel@midokura.com>

--- build.sh
+++ build.sh
@@ -18,13 +18,29 @@ if [ "${DOCKERACC}" = "" ]; then
 	DOCKERACC=midonet
 fi
 
-for VARIANT in amd64-linux arm64v8-linux; do
+VARIANTS="amd64-linux arm64v8-linux"
+
+for VARIANT in $VARIANTS; do
+        #
+        # Partial build to run unit tests and fetch JUnit report file (despite succeded or not)
+        #
+        # Includes 'trick' (see https://stackoverflow.com/a/49891339) to
+        # bypass Docker limitation about copy files from build-time containers.
+        #
+	docker build --target builder -t builder-${VARIANT} .
+	BUILDER_CONTAINER="$(docker run -d builder-${VARIANT})"
+	docker cp "$BUILDER_CONTAINER:/tmp/junit.xml" junit-${VARIANT}.xml
+	echo "Generated JUnit XML report for '${VARIANT}' variant: junit-${VARIANT}.xml"
+	docker rm -f "$BUILDER_CONTAINER"
+done
+
+for VARIANT in $VARIANTS; do
 	docker build -f Dockerfile-${VARIANT} -t ${DOCKERACC}/midonet-kube-controllers-${VARIANT}:${TAG} .
 	docker build -f Dockerfile-node-${VARIANT} -t ${DOCKERACC}/midonet-kube-node-${VARIANT}:${TAG} .
 done
 
 echo "Now you can push images with the following commands:"
-for VARIANT in amd64-linux arm64v8-linux; do
+for VARIANT in $VARIANTS; do
 	echo "  docker push ${DOCKERACC}/midonet-kube-controllers-${VARIANT}:${TAG}"
 	echo "  docker push ${DOCKERACC}/midonet-kube-node-${VARIANT}:${TAG}"
 done
